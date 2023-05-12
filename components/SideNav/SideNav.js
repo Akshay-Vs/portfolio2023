2500b53cdaf4f071bcea1b1f61eacaa5ba28e06c
Akshay-Vs <akshay.vs2005@gmail.com>
2023-05-12 09:26:08 +0530

    Added insigts

--- components/SideNav/SideNav.js
+++ components/SideNav/SideNav.js
@@ -5,7 +5,7 @@ import './SideNav.scss'
 export const SideNav = () => {
   return (
     <div className='nav-container'>
-        <div className='nav-buttons'>
+        <div className='nav-buttons active'>
           <i className="fa-solid fa-house" />
           <h1>Home</h1>
       </div>
@@ -20,7 +20,7 @@ export const SideNav = () => {
         <h1>Projects</h1>
       </div>
 
-      <div className='nav-buttons active'>
+      <div className='nav-buttons'>
         <i className="fa-solid fa-address-book" />
         <h1>Contact</h1>
       </div>
